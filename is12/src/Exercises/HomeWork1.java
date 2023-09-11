a58c252ef229c2a07e4c0180ac4804f979c48ef5
Pavel <103637313+HuntTTT@users.noreply.github.com>
2023-09-11 21:05:40 +1000

    Дз

--- is12/src/Exercises/HomeWork1.java
+++ is12/src/Exercises/HomeWork1.java
@@ -2,6 +2,7 @@ package Exercises;
 
 public class HomeWork1 {
     public static void main(String[] args) {
+
         System.out.println("  /\\_/\\      ----- ");
         System.out.println(" ( ' ' )   / Hello \\");
         System.out.println(" (  -  ) <   Junior |");
