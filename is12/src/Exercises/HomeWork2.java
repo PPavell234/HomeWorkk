4af7f502d62a9bfb58f2ac4975617325952fddf8
Pavel <103637313+HuntTTT@users.noreply.github.com>
2023-09-11 20:38:27 +1000

    Дз

---
+++ is12/src/Exercises/HomeWork2.java
@@ -0,0 +1,12 @@
+package Exercises;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+        System.out.println("  /////       ");
+        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println("(| o o |)");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '-' | ");
+        System.out.println(" +\"\"\"\"\"+ ");
+    }
+}
