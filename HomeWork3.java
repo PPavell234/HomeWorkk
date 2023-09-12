f3cbe5fd1b10df17935cf61ad4eb637143e13bc6
PPavell234 <144679343+PPavell234@users.noreply.github.com>
2023-09-12 23:22:46 +1000

    дз
    ва

---
+++ HomeWork3.java
@@ -0,0 +1,12 @@
+package Exercises;
+
+public class HomeWork3 {
+    public static void main(String[] args) {
+        System.out.println("*   *"+"    **"+"    ****"+"    ***** "+"  *   *  ");
+        System.out.println("*   *"+"   *  *"+"   *    *"+"  *    *"+"  *   *  " );
+        System.out.println("*****"+"  *    *"+"  *****"+"   ***** "+"  *   *  ");
+        System.out.println("*   *"+"  ******"+"  *   *"+"   *   * "+"    *    ");
+        System.out.println("*   *"+"  *    *"+"  *    *"+"  *    *"+"    *    ");
+
+    }
+}
