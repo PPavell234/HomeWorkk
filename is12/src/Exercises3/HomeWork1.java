49471f0d348a7ecea20bbca9bcdb336908163760
Pavel <103637313+HuntTTT@users.noreply.github.com>
2023-09-28 19:34:44 +1000

    Дз
    Добавил Перевод систем счисления + имя на Греческом яз

---
+++ is12/src/Exercises3/HomeWork1.java
@@ -0,0 +1,20 @@
+package Exercises3;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        byte byteDec = 12;
+        byte byteBin = 0b1100;
+        byte byteOct = 014;
+        byte byteHex = 0xC;
+
+        System.out.println("Десятичная = "+ byteDec);
+        System.out.println("Двоичная = "+ byteBin);
+        System.out.println("Восьмеричная = "+ byteOct);
+        System.out.println("Шестнадцатеричная = "+ byteHex);
+
+    }
+}
+
+
+
+
