de5d8d2554796baa6cd8bafb720be72bbf4db15b
PPavell234 <144679343+PPavell234@users.noreply.github.com>
2023-10-22 22:27:28 +1000

    Дз6

---
+++ is12/src/Exercises6/StudentTest.java
@@ -0,0 +1,16 @@
+package Exercises6;
+
+public class StudentTest {
+    public static void main(String[] args) {
+
+        // 3 объекта
+        Stydent student1 = new Stydent("000001","Алеша","Алексей",2,7,8,9);
+        Stydent student2 = new Stydent("000002","Попов","Петров",3,8,9,10);
+        Stydent student3 = new Stydent("000003","Максим","Сергеев",2,9,10,11);
+
+        System.out.println(student1);
+        System.out.println(student2);
+        System.out.println(student3);
+
+    }
+}
