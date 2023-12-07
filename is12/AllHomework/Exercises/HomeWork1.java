0adb148ad934f26d2fcfd036a667e54877515360
Pavel <varvardidl@gmail.com>
2023-12-07 19:07:40 +1000

    5

---
+++ is12/AllHomework/Exercises/HomeWork1.java
@@ -0,0 +1,16 @@
+package Exercises;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        System.out.println("  /\\_/\\      ----- ");
+        System.out.println(" ( ' ' )   / Hello \\");
+        System.out.println(" (  -  ) <   Junior |");
+        System.out.println("  | | |    \\ Coder!/");
+        System.out.println(" (__|__)     -----   ");
+
+
+        }
+
+
+
+    }
