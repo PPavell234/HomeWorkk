6d45c0253cdbc6f4cea8c43d51668e4d60b992fb
Pavel <varvardidl@gmail.com>
2024-09-19 01:16:57 +1000

    Задания

---
+++ is12/src/BookTask/STpri.java
@@ -0,0 +1,62 @@
+package BookTask;
+
+import labs.Practice3part1.SodaCan;
+
+import java.util.logging.Level;
+
+public class STpri {
+
+
+
+
+
+
+
+    Module moduleOne;
+
+    public Module getModuleOne() {
+        return moduleOne;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+        // print the value as long
+
+        // number as double
+
+
+
+
+
+
+        long number1 = 18;
+        long number2 = 21;
+
+        int resultNumber = Long.compareUnsigned(number1, number2);
+        System.out.println("Result: " + resultNumber);
+
+        double newDow = number2.
+        long val = 88L;
+        String result = Long.toString(val);
+        int error = 101;
+        int a = 5;
+        int b = 6;
+        if (a < b) {
+            System.exit(101);
+        } else {
+            System.out.println("Ошибки нету");
+        }
+
+
+        long time = System.currentTimeMillis();
+        System.out.println("Текущее время" + time);
+
+
+
+    }
+}
---
+++ is12/src/Chapter5/NumberNumber.java
@@ -0,0 +1,22 @@
+package Chapter5;
+
+import labs.Practice3part1.SodaCan;
+
+public class NumberNumber {
+
+
+
+    int number1 = 16;
+    int number2 = 18;
+
+    int resultNumber = Long.compareUnsigned(number1,number2);
+
+
+
+
+}
+
+
+
+
+
